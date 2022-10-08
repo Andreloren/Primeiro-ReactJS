aeb04080e872753a06f8f789a588b14a44e4ff23
Andreloren <andre.lorente83@gmail.com>
2022-10-08 06:57:14 -0300

    Global e Themes

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import MyApp from "./styles/themes/Light";
+import Global from "./styles/Global";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Global />
     <App />
   </React.StrictMode>
 );
